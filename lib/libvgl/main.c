probonopd/freebsd

commit cc381cc0689e6fca2122bff9e543bebc95e6bcae

Fix some races and screeen clearing in VGLEnd().
The mouse signal SIGUSR2 was not turned off for normal termination and
in some other cases.  Thus mouse signals arriving after the frame
buffer was unmapped always caused fatal traps.  The fatal traps occurred
about 1 time in 5 if the mouse was wiggled while vgl is ending.

The screen switch signal SIGUSR1 was turned off after clearing the
flag that it sets.  Unlike the mouse signal, this signal is handled
synchronously, but VGLEnd() does screen clearing which does the
synchronous handling.  This race is harder to lose.  I think it can
get vgl into deadlocked state (waiting in the screen switch handler
with SIGUSR1 to leave that state already turned off).

Turn off the mouse cursor before clearing the screen in VGLEnd().
Otherwise, clearing is careful to not clear the mouse cursor.  Undrawing
an active mouse cursor uses a lot of state, so is dangerous for abnormal
termination, but so is clearing.  Clearing is slow and is usually not
needed, since the kernel also does it (not quite right).


git-svn-id: svn+ssh://svn.freebsd.org/base/head@346631 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

lib/libvgl/main.c
@@ -73,11 +73,11 @@ struct vt_mode smode;
 
   if (!VGLInitDone)
     return;
-  VGLInitDone = 0;
+  signal(SIGUSR1, SIG_IGN);
+  signal(SIGUSR2, SIG_IGN);
   VGLSwitchPending = 0;
   VGLAbortPending = 0;
-
-  signal(SIGUSR1, SIG_IGN);
+  VGLMousePointerHide();
 
   if (VGLMem != MAP_FAILED) {
     VGLClear(VGLDisplay, 0);

commit 0235bf3b5415f611ee841ad96c76c7ddd034e34b

Restore doing nothing for calls to VGLEnd() after the first. I broke this in r346631.  VGLEnd() clears some state variables as it restores state, but not all of them, so it still needs to clear a single state variable to indicate that it has completed.  Put this clearing back where it was (at the start instead of the end) to avoid moving bugs in the signal handling.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@346678 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

lib/libvgl/main.c
@@ -73,6 +73,7 @@ struct vt_mode smode;
 
   if (!VGLInitDone)
     return;
+  VGLInitDone = 0;
   signal(SIGUSR1, SIG_IGN);
   signal(SIGUSR2, SIG_IGN);
   VGLSwitchPending = 0;