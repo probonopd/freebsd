probonopd/freebsd

commit 6fa1ab5305f2588c3af570d9bdbdeaede649b9d4

Convert route caching to nexthop caching.
This change is build on top of nexthop objects introduced in r359823.

Nexthops are separate datastructures, containing all necessary information
 to perform packet forwarding such as gateway interface and mtu. Nexthops
 are shared among the routes, providing more pre-computed cache-efficient
 data while requiring less memory. Splitting the LPM code and the attached
 data solves multiple long-standing problems in the routing layer,
 drastically reduces the coupling with outher parts of the stack and allows
 to transparently introduce faster lookup algorithms.

Route caching was (re)introduced to minimise (slow) routing lookups, allowing
 for notably better performance for large TCP senders. Caching works by
 acquiring rtentry reference, which is protected by per-rtentry mutex.
 If the routing table is changed (checked by comparing the rtable generation id)
 or link goes down, cache record gets withdrawn.

Nexthops have the same reference counting interface, backed by refcount(9).
This change merely replaces rtentry with the actual forwarding nextop as a
 cached object, which is mostly mechanical. Other moving parts like cache
 cleanup on rtable change remains the same.

Differential Revision:	https://reviews.freebsd.org/D24340


git-svn-id: svn+ssh://svn.freebsd.org/base/head@360292 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/netinet6/in6_src.c
@@ -91,6 +91,7 @@ __FBSDID("$FreeBSD$");
 #include <net/if_var.h>
 #include <net/if_dl.h>
 #include <net/route.h>
+#include <net/route/nhop.h>
 #include <net/if_llatbl.h>
 #ifdef RADIX_MPATH
 #include <net/radix_mpath.h>
@@ -134,7 +135,7 @@ VNET_DEFINE(int, ip6_prefer_tempaddr) = 0;
 
 static int selectroute(struct sockaddr_in6 *, struct ip6_pktopts *,
 	struct ip6_moptions *, struct route_in6 *, struct ifnet **,
-	struct rtentry **, int, u_int);
+	struct nhop_object **, int, u_int, uint32_t);
 static int in6_selectif(struct sockaddr_in6 *, struct ip6_pktopts *,
 	struct ip6_moptions *, struct ifnet **,
 	struct ifnet *, u_int);
@@ -625,11 +626,12 @@ in6_selectsrc_addr(uint32_t fibnum, const struct in6_addr *dst,
 static int
 selectroute(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
     struct ip6_moptions *mopts, struct route_in6 *ro,
-    struct ifnet **retifp, struct rtentry **retrt, int norouteok, u_int fibnum)
+    struct ifnet **retifp, struct nhop_object **retnh, int norouteok,
+    u_int fibnum, uint32_t flowid)
 {
 	int error = 0;
 	struct ifnet *ifp = NULL;
-	struct rtentry *rt = NULL;
+	struct nhop_object *nh = NULL;
 	struct sockaddr_in6 *sin6_next;
 	struct in6_pktinfo *pi = NULL;
 	struct in6_addr *dst = &dstsock->sin6_addr;
@@ -654,7 +656,7 @@ selectroute(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 		/* XXX boundary check is assumed to be already done. */
 		ifp = ifnet_byindex(pi->ipi6_ifindex);
 		if (ifp != NULL &&
-		    (norouteok || retrt == NULL ||
+		    (norouteok || retnh == NULL ||
 		    IN6_IS_ADDR_MULTICAST(dst))) {
 			/*
 			 * we do not have to check or get the route for
@@ -707,26 +709,31 @@ selectroute(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 		}
 		ron = &opts->ip6po_nextroute;
 		/* Use a cached route if it exists and is valid. */
-		if (ron->ro_rt != NULL && (
-		    (ron->ro_rt->rt_flags & RTF_UP) == 0 ||
+		if (ron->ro_nh != NULL && (
+		    !NH_IS_VALID(ron->ro_nh) ||
 		    ron->ro_dst.sin6_family != AF_INET6 ||
 		    !IN6_ARE_ADDR_EQUAL(&ron->ro_dst.sin6_addr,
 			&sin6_next->sin6_addr)))
-			RO_RTFREE(ron);
-		if (ron->ro_rt == NULL) {
+			RO_NHFREE(ron);
+		if (ron->ro_nh == NULL) {
 			ron->ro_dst = *sin6_next;
-			in6_rtalloc(ron, fibnum); /* multi path case? */
+			/*
+			 * sin6_next is not link-local OR scopeid is 0,
+			 * no need to clear scope
+			 */
+			ron->ro_nh = fib6_lookup(fibnum,
+			    &sin6_next->sin6_addr, 0, NHR_REF, flowid);
 		}
 		/*
 		 * The node identified by that address must be a
 		 * neighbor of the sending host.
 		 */
-		if (ron->ro_rt == NULL ||
-		    (ron->ro_rt->rt_flags & RTF_GATEWAY) != 0)
+		if (ron->ro_nh == NULL ||
+		    (ron->ro_nh->nh_flags & NHF_GATEWAY) != 0)
 			error = EHOSTUNREACH;
 		else {
-			rt = ron->ro_rt;
-			ifp = rt->rt_ifp;
+			nh = ron->ro_nh;
+			ifp = nh->nh_ifp;
 		}
 		goto done;
 	}
@@ -737,15 +744,14 @@ selectroute(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 	 * cached destination, in case of sharing the cache with IPv4.
 	 */
 	if (ro) {
-		if (ro->ro_rt &&
-		    (!(ro->ro_rt->rt_flags & RTF_UP) ||
+		if (ro->ro_nh &&
+		    (!NH_IS_VALID(ro->ro_nh) ||
 		     ((struct sockaddr *)(&ro->ro_dst))->sa_family != AF_INET6 ||
 		     !IN6_ARE_ADDR_EQUAL(&satosin6(&ro->ro_dst)->sin6_addr,
 		     dst))) {
-			RTFREE(ro->ro_rt);
-			ro->ro_rt = (struct rtentry *)NULL;
+			RO_NHFREE(ro);
 		}
-		if (ro->ro_rt == (struct rtentry *)NULL) {
+		if (ro->ro_nh == (struct nhop_object *)NULL) {
 			struct sockaddr_in6 *sa6;
 
 			/* No route yet, so try to acquire one */
@@ -754,15 +760,28 @@ selectroute(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 			*sa6 = *dstsock;
 			sa6->sin6_scope_id = 0;
 
+			/*
+			 * Currently dst has scopeid embedded iff it is LL.
+			 * New routing API accepts scopeid as a separate argument.
+			 * Convert dst before/after doing lookup
+			 */
+			uint32_t scopeid = 0;
+			if (IN6_IS_SCOPE_LINKLOCAL(&sa6->sin6_addr)) {
+				/* Unwrap in6_getscope() and in6_clearscope() */
+				scopeid = ntohs(sa6->sin6_addr.s6_addr16[1]);
+				sa6->sin6_addr.s6_addr16[1] = 0;
+
+			}
+
 #ifdef RADIX_MPATH
 				rtalloc_mpath_fib((struct route *)ro,
 				    ntohl(sa6->sin6_addr.s6_addr32[3]), fibnum);
 #else			
-				ro->ro_rt = in6_rtalloc1((struct sockaddr *)
-				    &ro->ro_dst, 0, 0UL, fibnum);
-				if (ro->ro_rt)
-					RT_UNLOCK(ro->ro_rt);
+			ro->ro_nh = fib6_lookup(fibnum,
+			    &sa6->sin6_addr, scopeid, NHR_REF, flowid);
 #endif
+			if (IN6_IS_SCOPE_LINKLOCAL(&sa6->sin6_addr))
+				sa6->sin6_addr.s6_addr16[1] = htons(scopeid);
 		}
 				
 		/*
@@ -772,17 +791,11 @@ selectroute(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 		if (opts && opts->ip6po_nexthop)
 			goto done;
 
-		if (ro->ro_rt) {
-			ifp = ro->ro_rt->rt_ifp;
-
-			if (ifp == NULL) { /* can this really happen? */
-				RTFREE(ro->ro_rt);
-				ro->ro_rt = NULL;
-			}
-		}
-		if (ro->ro_rt == NULL)
+		if (ro->ro_nh)
+			ifp = ro->ro_nh->nh_ifp;
+		else
 			error = EHOSTUNREACH;
-		rt = ro->ro_rt;
+		nh = ro->ro_nh;
 
 		/*
 		 * Check if the outgoing interface conflicts with
@@ -803,7 +816,7 @@ selectroute(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 	}
 
   done:
-	if (ifp == NULL && rt == NULL) {
+	if (ifp == NULL && nh == NULL) {
 		/*
 		 * This can happen if the caller did not pass a cached route
 		 * nor any other hints.  We treat this case an error.
@@ -814,26 +827,14 @@ selectroute(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 		IP6STAT_INC(ip6s_noroute);
 
 	if (retifp != NULL) {
-		*retifp = ifp;
-
-		/*
-		 * Adjust the "outgoing" interface.  If we're going to loop 
-		 * the packet back to ourselves, the ifp would be the loopback 
-		 * interface. However, we'd rather know the interface associated 
-		 * to the destination address (which should probably be one of 
-		 * our own addresses.)
-		 */
-		if (rt) {
-			if ((rt->rt_ifp->if_flags & IFF_LOOPBACK) &&
-			    (rt->rt_gateway->sa_family == AF_LINK))
-				*retifp = 
-					ifnet_byindex(((struct sockaddr_dl *)
-						       rt->rt_gateway)->sdl_index);
-		}
+		if (nh != NULL)
+			*retifp = nh->nh_aifp;
+		else
+			*retifp = ifp;
 	}
 
-	if (retrt != NULL)
-		*retrt = rt;	/* rt may be NULL */
+	if (retnh != NULL)
+		*retnh = nh;	/* nh may be NULL */
 
 	return (error);
 }
@@ -845,20 +846,20 @@ in6_selectif(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 {
 	int error;
 	struct route_in6 sro;
-	struct rtentry *rt = NULL;
-	int rt_flags;
+	struct nhop_object *nh = NULL;
+	uint16_t nh_flags;
 
 	KASSERT(retifp != NULL, ("%s: retifp is NULL", __func__));
 
 	bzero(&sro, sizeof(sro));
-	rt_flags = 0;
+	nh_flags = 0;
 
-	error = selectroute(dstsock, opts, mopts, &sro, retifp, &rt, 1, fibnum);
+	error = selectroute(dstsock, opts, mopts, &sro, retifp, &nh, 1, fibnum, 0);
 
-	if (rt)
-		rt_flags = rt->rt_flags;
-	if (rt && rt == sro.ro_rt)
-		RTFREE(rt);
+	if (nh != NULL)
+		nh_flags = nh->nh_flags;
+	if (nh != NULL && nh == sro.ro_nh)
+		NH_FREE(nh);
 
 	if (error != 0) {
 		/* Help ND. See oifp comment in in6_selectsrc(). */
@@ -887,8 +888,8 @@ in6_selectif(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 	 * We thus reject the case here.
 	 */
 
-	if (rt_flags & (RTF_REJECT | RTF_BLACKHOLE)) {
-		error = (rt_flags & RTF_HOST ? EHOSTUNREACH : ENETUNREACH);
+	if (nh_flags & (NHF_REJECT | NHF_BLACKHOLE)) {
+		error = (nh_flags & NHF_HOST ? EHOSTUNREACH : ENETUNREACH);
 		return (error);
 	}
 
@@ -899,11 +900,11 @@ in6_selectif(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 int
 in6_selectroute(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
     struct ip6_moptions *mopts, struct route_in6 *ro,
-    struct ifnet **retifp, struct rtentry **retrt, u_int fibnum)
+    struct ifnet **retifp, struct nhop_object **retnh, u_int fibnum, uint32_t flowid)
 {
 
 	return (selectroute(dstsock, opts, mopts, ro, retifp,
-	    retrt, 0, fibnum));
+	    retnh, 0, fibnum, flowid));
 }
 
 /*

sys/kern/uipc_ktls.c
@@ -58,6 +58,7 @@ __FBSDID("$FreeBSD$");
 #include <net/if_var.h>
 #ifdef RSS
 #include <net/netisr.h>
+#include <net/nhop.h>
 #include <net/rss_config.h>
 #endif
 #if defined(INET) || defined(INET6)
@@ -754,7 +755,7 @@ ktls_alloc_snd_tag(struct inpcb *inp, struct ktls_session *tls, bool force,
 {
 	union if_snd_tag_alloc_params params;
 	struct ifnet *ifp;
-	struct rtentry *rt;
+	struct nhop_object *nh;
 	struct tcpcb *tp;
 	int error;
 
@@ -792,12 +793,12 @@ ktls_alloc_snd_tag(struct inpcb *inp, struct ktls_session *tls, bool force,
 	 * enabled after a connection has completed key negotiation in
 	 * userland, the cached route will be present in practice.
 	 */
-	rt = inp->inp_route.ro_rt;
-	if (rt == NULL || rt->rt_ifp == NULL) {
+	nh = inp->inp_route.ro_nh;
+	if (nh == NULL) {
 		INP_RUNLOCK(inp);
 		return (ENXIO);
 	}
-	ifp = rt->rt_ifp;
+	ifp = nh->nh_ifp;
 	if_ref(ifp);
 
 	params.hdr.type = IF_SND_TAG_TYPE_TLS;

sys/net/radix_mpath.c
@@ -55,6 +55,8 @@ __FBSDID("$FreeBSD$");
 #include <net/radix_mpath.h>
 #include <sys/rmlock.h>
 #include <net/route.h>
+#include <net/route/nhop.h>
+#include <net/route/shared.h>
 #include <net/route_var.h>
 #include <net/route/nhop.h>
 #include <net/if.h>
@@ -257,42 +259,41 @@ rt_mpath_select(struct rtentry *rte, uint32_t hash)
 void
 rtalloc_mpath_fib(struct route *ro, uint32_t hash, u_int fibnum)
 {
-	struct rtentry *rt;
+	struct rtentry *rt, *rt_tmp;
 
 	/*
 	 * XXX we don't attempt to lookup cached route again; what should
 	 * be done for sendto(3) case?
 	 */
-	if (ro->ro_rt && ro->ro_rt->rt_ifp && (ro->ro_rt->rt_flags & RTF_UP)
-	    && RT_LINK_IS_UP(ro->ro_rt->rt_ifp))
+	if (ro->ro_nh && RT_LINK_IS_UP(ro->ro_nh->nh_ifp))
 		return;				 
-	ro->ro_rt = rtalloc1_fib(&ro->ro_dst, 1, 0, fibnum);
+	ro->ro_nh = NULL;
+	rt_tmp = rtalloc1_fib(&ro->ro_dst, 1, 0, fibnum);
 
 	/* if the route does not exist or it is not multipath, don't care */
-	if (ro->ro_rt == NULL)
+	if (rt_tmp == NULL)
 		return;
-	if (rn_mpath_next((struct radix_node *)ro->ro_rt) == NULL) {
-		RT_UNLOCK(ro->ro_rt);
+	if (rn_mpath_next((struct radix_node *)rt_tmp) == NULL) {
+		ro->ro_nh = rt_tmp->rt_nhop;
+		nhop_ref_object(ro->ro_nh);
+		RT_UNLOCK(rt_tmp);
 		return;
 	}
 
-	rt = rt_mpath_selectrte(ro->ro_rt, hash);
+	rt = rt_mpath_selectrte(rt_tmp, hash);
 	/* XXX try filling rt_gwroute and avoid unreachable gw  */
 
 	/* gw selection has failed - there must be only zero weight routes */
 	if (!rt) {
-		RT_UNLOCK(ro->ro_rt);
-		ro->ro_rt = NULL;
+		RT_UNLOCK(rt_tmp);
 		return;
 	}
-	if (ro->ro_rt != rt) {
-		RTFREE_LOCKED(ro->ro_rt);
-		ro->ro_rt = rt;
-		RT_LOCK(ro->ro_rt);
-		RT_ADDREF(ro->ro_rt);
-
-	} 
-	RT_UNLOCK(ro->ro_rt);
+	if (rt_tmp != rt) {
+		RTFREE_LOCKED(rt_tmp);
+		ro->ro_nh = rt->rt_nhop;
+		nhop_ref_object(ro->ro_nh);
+	} else
+		RT_UNLOCK(rt_tmp);
 }
 
 void

sys/net/route.c
@@ -441,15 +441,18 @@ rtalloc_ign_fib(struct route *ro, u_long ignore, u_int fibnum)
 {
 	struct rtentry *rt;
 
-	if ((rt = ro->ro_rt) != NULL) {
-		if (rt->rt_ifp != NULL && rt->rt_flags & RTF_UP)
+	if (ro->ro_nh != NULL) {
+		if (NH_IS_VALID(ro->ro_nh))
 			return;
-		RTFREE(rt);
-		ro->ro_rt = NULL;
+		NH_FREE(ro->ro_nh);
+		ro->ro_nh = NULL;
 	}
-	ro->ro_rt = rtalloc1_fib(&ro->ro_dst, 1, ignore, fibnum);
-	if (ro->ro_rt)
-		RT_UNLOCK(ro->ro_rt);
+	rt = rtalloc1_fib(&ro->ro_dst, 1, ignore, fibnum);
+	if (rt != NULL) {
+		ro->ro_nh = rt->rt_nhop;
+		nhop_ref_object(rt->rt_nhop);
+		RT_UNLOCK(rt);
+	}
 }
 
 /*

sys/net/route.h
@@ -51,7 +51,7 @@
  * with its length.
  */
 struct route {
-	struct	rtentry *ro_rt;
+	struct	nhop_object *ro_nh;
 	struct	llentry *ro_lle;
 	/*
 	 * ro_prepend and ro_plen are only used for bpf to pass in a
@@ -226,21 +226,6 @@ struct rtentry {
 
 /* Control plane route request flags */
 #define	NHR_COPY		0x100	/* Copy rte data */
-
-#ifdef _KERNEL
-/* rte<>ro_flags translation */
-static inline void
-rt_update_ro_flags(struct route *ro)
-{
-	int rt_flags = ro->ro_rt->rt_flags;
-
-	ro->ro_flags &= ~ (RT_REJECT|RT_BLACKHOLE|RT_HAS_GW);
-
-	ro->ro_flags |= (rt_flags & RTF_REJECT) ? RT_REJECT : 0;
-	ro->ro_flags |= (rt_flags & RTF_BLACKHOLE) ? RT_BLACKHOLE : 0;
-	ro->ro_flags |= (rt_flags & RTF_GATEWAY) ? RT_HAS_GW : 0;
-}
-#endif
 
 /*
  * Routing statistics.
@@ -431,11 +416,21 @@ struct rt_addrinfo {
 		RTFREE((_ro)->ro_rt);				\
 } while (0)
 
+#define	RO_NHFREE(_ro) do {					\
+	if ((_ro)->ro_nh) {					\
+		NH_FREE((_ro)->ro_nh);				\
+		(_ro)->ro_nh = NULL;				\
+	}							\
+} while (0)
+
 #define	RO_INVALIDATE_CACHE(ro) do {					\
-		RO_RTFREE(ro);						\
 		if ((ro)->ro_lle != NULL) {				\
 			LLE_FREE((ro)->ro_lle);				\
 			(ro)->ro_lle = NULL;				\
+		}							\
+		if ((ro)->ro_nh != NULL) {				\
+			NH_FREE((ro)->ro_nh);				\
+			(ro)->ro_nh = NULL;				\
 		}							\
 	} while (0)
 
@@ -444,7 +439,7 @@ struct rt_addrinfo {
  * out-of-date cache, simply free it.  Update the generation number
  * for the new allocation
  */
-#define RT_VALIDATE(ro, cookiep, fibnum) do {				\
+#define NH_VALIDATE(ro, cookiep, fibnum) do {				\
 	rt_gen_t cookie = RT_GEN(fibnum, (ro)->ro_dst.sa_family);	\
 	if (*(cookiep) != cookie) {					\
 		RO_INVALIDATE_CACHE(ro);				\

sys/net/route_var.h
@@ -80,7 +80,7 @@ _Static_assert(__offsetof(_s1, _f1) == __offsetof(_s2, _f2),		\
 	CHK_STRUCT_FIELD_GENERIC(struct route, _field, _route_new, _field)
 
 #define CHK_STRUCT_ROUTE_FIELDS(_route_new)	\
-	_CHK_ROUTE_FIELD(_route_new, ro_rt)	\
+	_CHK_ROUTE_FIELD(_route_new, ro_nh)	\
 	_CHK_ROUTE_FIELD(_route_new, ro_lle)	\
 	_CHK_ROUTE_FIELD(_route_new, ro_prepend)\
 	_CHK_ROUTE_FIELD(_route_new, ro_plen)	\

sys/netinet/in_fib.c
@@ -62,6 +62,10 @@ __FBSDID("$FreeBSD$");
 #include <netinet/in_fib.h>
 
 #ifdef INET
+
+/* Verify struct route compatiblity */
+/* Assert 'struct route_in' is compatible with 'struct route' */
+CHK_STRUCT_ROUTE_COMPAT(struct route_in, ro_dst4);
 static void fib4_rte_to_nh_basic(struct nhop_object *nh, struct in_addr dst,
     uint32_t flags, struct nhop4_basic *pnh4);
 static void fib4_rte_to_nh_extended(struct nhop_object *nh, struct in_addr dst,

sys/netinet/in_fib.h
@@ -32,6 +32,19 @@
 #ifndef _NETINET_IN_FIB_H_
 #define	_NETINET_IN_FIB_H_
 
+struct route_in {
+	/* common fields shared among all 'struct route' */
+	struct nhop_object *ro_nh;
+	struct	llentry *ro_lle;
+	char		*ro_prepend;
+	uint16_t	ro_plen;
+	uint16_t	ro_flags;
+	uint16_t	ro_mtu;	/* saved ro_rt mtu */
+	uint16_t	spare;
+	/* custom sockaddr */
+	struct sockaddr_in ro_dst4;
+};
+
 /* Basic nexthop info used for uRPF/mtu checks */
 struct nhop4_basic {
 	struct ifnet	*nh_ifp;	/* Logical egress interface */

commit 0ae1e5e7de65d60a9ad078d12354396ca59a9052

Fix IPv6 link-local operations with RADIX_MPATH.
It was broken by r360292 as fib6_lookup() assumes de-embedded addresses
 while rtalloc_mpath_fib() requires sockaddr with embedded ones.

New fib6_lookup() transparently supports multipath, hence
 remove old RADIX_MPATH condition.


git-svn-id: svn+ssh://svn.freebsd.org/base/head@360348 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/netinet6/in6_src.c
@@ -93,9 +93,6 @@ __FBSDID("$FreeBSD$");
 #include <net/route.h>
 #include <net/route/nhop.h>
 #include <net/if_llatbl.h>
-#ifdef RADIX_MPATH
-#include <net/radix_mpath.h>
-#endif
 
 #include <netinet/in.h>
 #include <netinet/in_var.h>
@@ -773,13 +770,9 @@ selectroute(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 
 			}
 
-#ifdef RADIX_MPATH
-				rtalloc_mpath_fib((struct route *)ro,
-				    ntohl(sa6->sin6_addr.s6_addr32[3]), fibnum);
-#else			
 			ro->ro_nh = fib6_lookup(fibnum,
 			    &sa6->sin6_addr, scopeid, NHR_REF, flowid);
-#endif
+
 			if (IN6_IS_SCOPE_LINKLOCAL(&sa6->sin6_addr))
 				sa6->sin6_addr.s6_addr16[1] = htons(scopeid);
 		}